96a54b9983da72d39bd2dde353316ee3953911d7
Dmytro Herashchenko <stoczec@gmail.com>
2024-10-08 15:53:02 +0200

    "Wrap carousel and mode toggle in MaxWidthWrapper and add section element"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,31 +18,33 @@ import Image from 'next/image'
 
 import { data } from '@/data/data'
 import { ModeToggle } from '@/components/modeToggle'
+import { MaxWidthWrapper } from '@/components/MaxWidthWrapper'
 
 export default function Home() {
 	return (
-		<div
-			className="flex justify-center items-center mt-32 p-1
-		"
-		>
-			<ModeToggle />
-			<Carousel
-				opts={{
-					align: 'start',
-				}}
-				orientation="vertical"
-				className="w-full max-w-xs"
-			>
-				<CarouselContent className="-mt-1 h-[304px] items-center ">
-					{data.map(card => (
-						<CarouselItem key={card.id} className="pt-1">
-							<CardWord data={card} />
-						</CarouselItem>
-					))}
-				</CarouselContent>
-				<CarouselPrevious />
-				<CarouselNext />
-			</Carousel>
+		<div>
+			<section>
+				<MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-32 lg:pb-52">
+					<ModeToggle />
+					<Carousel
+						opts={{
+							align: 'start',
+						}}
+						orientation="vertical"
+						className="w-full max-w-xs"
+					>
+						<CarouselContent className="-mt-1 h-[304px] items-center ">
+							{data.map(card => (
+								<CarouselItem key={card.id} className="pt-1">
+									<CardWord data={card} />
+								</CarouselItem>
+							))}
+						</CarouselContent>
+						<CarouselPrevious />
+						<CarouselNext />
+					</Carousel>
+				</MaxWidthWrapper>
+			</section>
 		</div>
 	)
 }
