75b96a9bc8fede9d7ce54e639346ed76082911bd
Dmytro Herashchenko <stoczec@gmail.com>
2024-10-09 18:57:54 +0200

    Add search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
 	Carousel,
 	CarouselContent,
@@ -8,13 +10,32 @@ import {
 
 import { data } from '@/data/data'
 import { CardWord, MaxWidthWrapper } from '@/components'
-import style from 'styled-jsx/style'
+import { Input } from '@/components/ui/input'
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
+	const [searchQuery, setSearchQuery] = useState('')
+
+	// Фильтрация данных на основе поискового запроса
+	const filteredData = data.filter(
+		card =>
+			card.wordDe.toLowerCase().includes(searchQuery.toLowerCase()) ||
+			card.wordRu.toLowerCase().includes(searchQuery.toLowerCase())
+	)
 	return (
 		<div className="flex flex-col items-center justify-center flex-grow flex-shrink-0 basis-auto">
 			<section>
-				<MaxWidthWrapper className="flex py-5 sm:pb-16 lg:py-20">
+				<div className="flex gap-2 py-3">
+					<Input
+						type="text"
+						placeholder="Search"
+						value={searchQuery}
+						onChange={e => setSearchQuery(e.target.value)}
+					/>
+					<Button onClick={() => setSearchQuery('')}>Clear</Button>
+				</div>
+				<MaxWidthWrapper className="flex py-3 sm:py-6 lg:py-6">
 					<Carousel
 						opts={{
 							align: 'start',
@@ -23,7 +44,7 @@ export default function Home() {
 						className="w-full p-2"
 					>
 						<CarouselContent className="-mt-1 items-center h-[576px]">
-							{data.map(card => (
+							{filteredData.map(card => (
 								<CarouselItem key={card.id} className="pt-1">
 									<CardWord data={card} />
 								</CarouselItem>
