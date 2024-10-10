2748b39c57523c4141e21afe419e6eae921b7a80
Dmytro Herashchenko <stoczec@gmail.com>
2024-10-10 14:09:49 +0200

    "Update footer and navbar styles: added bg-black/5 to footer and navbar, changed navbar's bg-black/50 to bg-black/5"

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ import { MaxWidthWrapper } from './maxWidthWrapper'
 
 export const Navbar = async () => {
 	return (
-		<nav className="h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-black/50 backdrop-blur-lg transition-all">
+		<nav className="h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-black/5 backdrop-blur-lg transition-all">
 			<MaxWidthWrapper>
 				<div className="flex h-14 items-center justify-between">
 					<Link href="/" className="flex z-40 font-semibold">
