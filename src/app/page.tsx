776dd7f42f4f719c313e408960cbc13204d27b87
Dmytro Herashchenko <stoczec@gmail.com>
2024-10-11 12:32:11 +0200

    "Updated Home component: changed paragraph styles, added hyphen to title, moved button outside of div, and adjusted spacing"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,25 +8,27 @@ import Link from 'next/link'
 export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-between gap-4 py-3 flex-grow flex-shrink-0 basis-auto">
-			<div className="w-[320px] flex flex-col gap-3">
-				<p className="text-balance">
+			<div className="w-[320px] flex flex-col gap-4">
+				<p>
 					<span className="text-3xl font-bold">
-						wort<span className="text-primary">karte </span>
+						wort<span className="text-primary">karte </span>-{' '}
 					</span>
 					die den Nutzern dabei hilft, Deutsch zu lernen, indem sie Lernkarten
 					mit assoziativen Bildern und einer farblichen Kategorisierung der
 					Grammatik zur besseren Einprägung verwendet.
 				</p>
 				<Separator className="h-[2px] rounded-xl bg-gray-300" />
-				<p className="text-balance">
+				<p>
 					Auf der Seite werden Wörter je nach Wortklasse farblich hervorgehoben,
 					um ihre Kategorie visuell zu verdeutlichen.
 				</p>
 				<GrammarTable />
-				<Link href="/page/1">
-					<Button size={'lg'}>Los geht's!</Button>
-				</Link>
 			</div>
+			<Link href="/page/1">
+				<Button size={'lg'} className="text-lg">
+					Los geht's!
+				</Button>
+			</Link>
 			<Footer />
 		</section>
 	)
