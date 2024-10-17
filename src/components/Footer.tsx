c7d75f0f3dcbdebb0a042a35630e58e7325352e2
Dmytro Herashchenko <stoczec@gmail.com>
2024-10-18 01:41:14 +0200

    "Update footer and header heights from 14 to 12"

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
 	const { searchQuery, updateSearchQuery } = useFilteredCards()
 
 	return (
-		<footer className="w-full h-14 border-t border-gray-200 bg-black/5 flex-grow-0 flex-shrink-0 basis-auto">
+		<footer className="w-full h-12 border-t border-gray-200 bg-black/5 flex-grow-0 flex-shrink-0 basis-auto">
 			<MaxWidthWrapper className="flex justify-center items-center">
 				{/* <SearchBar
 					searchQuery={searchQuery}
