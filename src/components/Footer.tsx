f7f76169f338827f49e0eeb16d50848914b2e6e7
Dmytro Herashchenko <stoczec@gmail.com>
2024-10-17 23:58:03 +0200

    "Moved SearchBar from Header to Footer, removed card count displays from Footer"

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,18 @@
 'use client'
 
-import { useAllCardsStore, useFavoriteCardsStore } from '@/stores'
-import { MaxWidthWrapper } from '.'
+import { MaxWidthWrapper, SearchBar } from '.'
+import { useFilteredCards } from '@/hooks'
 
 export const Footer = () => {
-	const cards = useAllCardsStore(state => state.cards)
-	const favoriteCards = useFavoriteCardsStore(state => state.favoriteCards)
+	const { searchQuery, updateSearchQuery } = useFilteredCards()
+
 	return (
 		<footer className="w-full h-14 border-t border-gray-200 bg-black/5 flex-grow-0 flex-shrink-0 basis-auto">
-			<MaxWidthWrapper>
-				<div className="h-full flex flex-col justify-center items-center md:flex-row md:justify-between ">
-					<div>
-						<p className="text-sm text-muted-foreground">
-							Anzahl der Karten:{' '}
-							<span className="text-primary">{cards.length}</span> Stück
-						</p>
-					</div>
-
-					<div>
-						<p className="text-sm text-muted-foreground">
-							Anzahl deiner Favoritenkarten:{' '}
-							<span className="text-primary">{favoriteCards.length}</span> Stück
-						</p>
-					</div>
-				</div>
+			<MaxWidthWrapper className="flex justify-center items-center">
+				<SearchBar
+					searchQuery={searchQuery}
+					setSearchQuery={updateSearchQuery}
+				/>
 			</MaxWidthWrapper>
 		</footer>
 	)
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,16 @@
 'use client'
 
 import React from 'react'
-import { MaxWidthWrapper, BurgerMenu, SearchBar } from '.'
+import { MaxWidthWrapper, BurgerMenu } from '.'
 import Link from 'next/link'
-import { useFilteredCards } from '@/hooks'
 
 export const Header = () => {
-	const { searchQuery, updateSearchQuery } = useFilteredCards()
 	return (
 		<header className="h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-black/5 backdrop-blur-lg transition-all px-4">
 			<MaxWidthWrapper className="flex items-center justify-between gap-2 py-4">
 				<Link href="/" className="text-xl font-semibold">
 					wort<span className="text-primary">karte</span>
 				</Link>
-				<SearchBar
-					searchQuery={searchQuery}
-					setSearchQuery={updateSearchQuery}
-				/>
 				<BurgerMenu />
 			</MaxWidthWrapper>
 		</header>
