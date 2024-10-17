fb23a6f3650af2067cdd82d2e4ffa9c429651192
Dmytro Herashchenko <stoczec@gmail.com>
2024-10-17 22:44:49 +0200

    "Removed icons from header, added SearchBar component and useFilteredCards hook"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,22 @@
+'use client'
+
 import React from 'react'
-import { House, Heart, Search, GalleryHorizontalEnd } from 'lucide-react'
-import { MaxWidthWrapper, BurgerMenu } from '.'
+import { MaxWidthWrapper, BurgerMenu, SearchBar } from '.'
 import Link from 'next/link'
+import { useFilteredCards } from '@/hooks'
 
 export const Header = () => {
+	const { searchQuery, updateSearchQuery } = useFilteredCards()
 	return (
 		<header className="h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-black/5 backdrop-blur-lg transition-all px-4">
 			<MaxWidthWrapper className="flex items-center justify-between py-4">
 				<Link href="/" className="text-xl font-semibold">
 					wort<span className="text-primary">karte</span>
 				</Link>
-				<div className="flex items-center gap-4">
-					<Link href="/">
-						<House />
-					</Link>
-					<Link href="/page/1">
-						<GalleryHorizontalEnd />
-					</Link>
-					<Link href="/favorite/1">
-						<Heart />
-					</Link>
-					<Search />
-				</div>
+				<SearchBar
+					searchQuery={searchQuery}
+					setSearchQuery={updateSearchQuery}
+				/>
 				<BurgerMenu />
 			</MaxWidthWrapper>
 		</header>
