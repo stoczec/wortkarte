b849861367f42b51955e9c35fff57b1b72ab5cf1
Dmytro Herashchenko <stoczec@gmail.com>
2024-10-17 23:38:39 +0200

    "Updated Footer component to use two stores, rearranged and updated footer content, and removed unused links"

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,15 @@
 'use client'
 
-import { useAllCardsStore } from '@/stores'
+import { useAllCardsStore, useFavoriteCardsStore } from '@/stores'
 import { MaxWidthWrapper } from '.'
 
 export const Footer = () => {
 	const cards = useAllCardsStore(state => state.cards)
+	const favoriteCards = useFavoriteCardsStore(state => state.favoriteCards)
 	return (
 		<footer className="w-full h-14 border-t border-gray-200 bg-black/5 flex-grow-0 flex-shrink-0 basis-auto">
 			<MaxWidthWrapper>
 				<div className="h-full flex flex-col justify-center items-center md:flex-row md:justify-between ">
-					<div className="text-center md:text-left pb-2 md:pb-0">
-						<p className="text-sm text-muted-foreground">
-							&copy;
-							{new Date().getFullYear()}
-						</p>
-					</div>
-
 					<div>
 						<p className="text-sm text-muted-foreground">
 							Anzahl der Karten:{' '}
@@ -23,28 +17,12 @@ export const Footer = () => {
 						</p>
 					</div>
 
-					{/* <div className="flex items-center justify-center">
-						<div className="flex space-x-8">
-							<Link
-								href="#"
-								className="text-sm text-muted-foreground hover:text-gray-600"
-							>
-								Therms
-							</Link>
-							<Link
-								href="#"
-								className="text-sm text-muted-foreground hover:text-gray-600"
-							>
-								Privacy Policy
-							</Link>
-							<Link
-								href="#"
-								className="text-sm text-muted-foreground hover:text-gray-600"
-							>
-								Cookie Policy
-							</Link>
-						</div>
-					</div> */}
+					<div>
+						<p className="text-sm text-muted-foreground">
+							Anzahl deiner Favoritenkarten:{' '}
+							<span className="text-primary">{favoriteCards.length}</span> Stück
+						</p>
+					</div>
 				</div>
 			</MaxWidthWrapper>
 		</footer>
