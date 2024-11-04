1d4a96daf85fecda926a3de29cb8bd1a798fbe0a
Dmytro Herashchenko <stoczec@gmail.com>
2024-11-04 07:18:25 +0100

    "Refactor BurgerMenu component: remove Select and Badge imports, add CustomSelect import, and update component implementation"

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -9,17 +9,7 @@ import {
     SheetTitle,
     SheetTrigger,
 } from '@/components/ui/sheet'
-
-import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from '@/components/ui/select'
-import { Badge } from '@/components/ui/badge'
-import { ModeToggle, NavMenu } from '.'
+import { CustomSelect, ModeToggle, NavMenu } from '.'
 import { Separator } from './ui/separator'
 import { Menu } from 'lucide-react'
 import { useAllCardsStore } from '@/stores'
@@ -27,14 +17,6 @@ import { useAllCardsStore } from '@/stores'
 // const SHEET_SIDES = ['right'] as const
 
 export function BurgerMenu() {
-    const setItemsPerPage = useAllCardsStore(state => state.setItemsPerPage) // Получаем функцию установки itemsPerPage из Zustand
-    const itemsPerPage = useAllCardsStore(state => state.itemsPerPage)
-    console.log(itemsPerPage)
-
-    const handleSelectChange = (value: string) => {
-        console.log('value: ' + value)
-        setItemsPerPage(Number(value)) // Конвертируем значение в число и обновляем itemsPerPage
-    }
     return (
         <Sheet>
             <SheetTrigger asChild className="flex justify-center items-center">
@@ -52,27 +34,11 @@ export function BurgerMenu() {
 
                 {/* <ul className="grid gap-4 flex-grow"> */}
                 <NavMenu />
-                <Separator />
-                <div className="w-full flex justify-start gap-2 px-2">
-                    <Select onValueChange={handleSelectChange}>
-                        <SelectTrigger className="w-[260px]">
-                            <SelectValue placeholder="Die Anzahl der angezeigten Karten" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectGroup>
-                                <SelectItem value="5">5</SelectItem>
-                                <SelectItem value="10">10</SelectItem>
-                                <SelectItem value="15">15</SelectItem>
-                                <SelectItem value="20">20</SelectItem>
-                            </SelectGroup>
-                        </SelectContent>
-                    </Select>
-                    <Badge variant="default" className="bg-primary">
-                        {itemsPerPage} Stück
-                    </Badge>
-                </div>
 
                 <Separator />
+                <CustomSelect />
+                <Separator />
+
                 <div className="flex justify-between items-center px-2 text-sm flex-grow-0 flex-shrink-0">
                     <p>Theme</p>
                     <ModeToggle />
---
+++ src/components/CustomSelect.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select'
+import { Badge } from '@/components/ui/badge'
+import { useAllCardsStore } from '@/stores'
+
+export const CustomSelect = () => {
+    const setItemsPerPage = useAllCardsStore(state => state.setItemsPerPage)
+    const itemsPerPage = useAllCardsStore(state => state.itemsPerPage)
+    console.log(itemsPerPage)
+
+    const handleSelectChange = (value: string) => {
+        console.log('value: ' + value)
+        setItemsPerPage(Number(value))
+    }
+    return (
+        <div className="w-full flex justify-start gap-2 px-2">
+            <Select onValueChange={handleSelectChange}>
+                <SelectTrigger className="w-[260px]">
+                    <SelectValue placeholder="Die Anzahl der angezeigten Karten" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectGroup>
+                        <SelectItem value="5">5</SelectItem>
+                        <SelectItem value="10">10</SelectItem>
+                        <SelectItem value="15">15</SelectItem>
+                        <SelectItem value="20">20</SelectItem>
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
+            <Badge variant="default" className="bg-primary">
+                {itemsPerPage} Stück
+            </Badge>
+        </div>
+    )
+}
