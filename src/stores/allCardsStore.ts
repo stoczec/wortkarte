82bd89271ad2df4d111476b42802d3c20f07db10
Dmytro Herashchenko <stoczec@gmail.com>
2024-11-04 06:47:44 +0100

    "Updated interfaces and allCardsStore with minor formatting changes and added setItemsPerPage method to IAllCardsStore"

--- src/stores/allCardsStore.ts
+++ src/stores/allCardsStore.ts
@@ -4,20 +4,21 @@ import { IAllCardsStore, ILanguageCard } from '@/interfaces/interfaces'
 
 // Функция для перемешивания массива
 function shuffleArray(array: ILanguageCard[]) {
-	const shuffled = array.slice()
-	for (let i = shuffled.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1))
-		;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
-	}
-	return shuffled
+    const shuffled = array.slice()
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+    }
+    return shuffled
 }
 
 const shuffled = shuffleArray(data)
 
 export const useAllCardsStore = create<IAllCardsStore>(set => ({
-	cards: data.flatMap(card => [card, ...(card.multiple || [])]),
-	loading: true,
-	itemsPerPage: 5,
-	setLoading: isLoading => set({ loading: isLoading }),
-	shuffledCards: shuffled.flatMap(card => [card, ...(card.multiple || [])]),
+    cards: data.flatMap(card => [card, ...(card.multiple || [])]),
+    loading: true,
+    itemsPerPage: 5,
+    setLoading: isLoading => set({ loading: isLoading }),
+    shuffledCards: shuffled.flatMap(card => [card, ...(card.multiple || [])]),
+    setItemsPerPage: items => set({ itemsPerPage: items }),
 }))
