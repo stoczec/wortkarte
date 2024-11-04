055151ea45bc3ab8d86387528808d40b1ace1347
Dmytro Herashchenko <stoczec@gmail.com>
2024-11-04 07:10:47 +0100

    "Added persist middleware to allCardsStore to enable local storage, with 'all-cards-storage' key and partial persistence of itemsPerPage"

--- src/stores/allCardsStore.ts
+++ src/stores/allCardsStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 import { data } from '@/data/data'
 import { IAllCardsStore, ILanguageCard } from '@/interfaces/interfaces'
 
@@ -14,11 +15,22 @@ function shuffleArray(array: ILanguageCard[]) {
 
 const shuffled = shuffleArray(data)
 
-export const useAllCardsStore = create<IAllCardsStore>(set => ({
-    cards: data.flatMap(card => [card, ...(card.multiple || [])]),
-    loading: true,
-    itemsPerPage: 5,
-    setLoading: isLoading => set({ loading: isLoading }),
-    shuffledCards: shuffled.flatMap(card => [card, ...(card.multiple || [])]),
-    setItemsPerPage: items => set({ itemsPerPage: items }),
-}))
+export const useAllCardsStore = create<IAllCardsStore>()(
+    persist(
+        set => ({
+            cards: data.flatMap(card => [card, ...(card.multiple || [])]),
+            loading: true,
+            itemsPerPage: 5,
+            setLoading: isLoading => set({ loading: isLoading }),
+            shuffledCards: shuffled.flatMap(card => [
+                card,
+                ...(card.multiple || []),
+            ]),
+            setItemsPerPage: items => set({ itemsPerPage: items }),
+        }),
+        {
+            name: 'all-cards-storage', // Название ключа в локальном хранилище
+            partialize: state => ({ itemsPerPage: state.itemsPerPage }), // Сохраняем только itemsPerPage
+        }
+    )
+)
