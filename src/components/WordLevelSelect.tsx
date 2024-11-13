dec7f5edc9ee5f1eb479859c204a090118e08152
Dmytro Herashchenko <stoczec@gmail.com>
2024-11-13 09:57:43 +0100

    "Added card count display to WordLevelSelect component"

--- src/components/WordLevelSelect.tsx
+++ src/components/WordLevelSelect.tsx
@@ -13,6 +13,8 @@ import { EnumWORDLEVELS } from '@/enums/enums'
 import { useAllCardsStore } from '@/stores'
 import { Badge } from './ui/badge'
 import { useRouter } from 'next/navigation'
+import { ILanguageCard } from '@/interfaces/interfaces'
+import { A2_B2_data, C1_Beruf_data, C1_Sicher_data } from '@/data'
 
 export const WordLevelSelect = () => {
     const { selectedLevel, setSelectedLevel } = useAllCardsStore()
@@ -24,17 +26,50 @@ export const WordLevelSelect = () => {
         router.push('/page/1')
     }
 
+    function countTotalCards(cards: ILanguageCard[]): number {
+        let count = 0
+
+        for (const card of cards) {
+            count++
+
+            if (card.multiple) {
+                count += countTotalCards(card.multiple)
+            }
+        }
+
+        return count
+    }
+
+    const totalA2B2Cards = countTotalCards(A2_B2_data)
+    const totalC1SicherCards = countTotalCards(C1_Sicher_data)
+    const totalC1BerufCards = countTotalCards(C1_Beruf_data)
+
     return (
         <div className="w-full flex justify-start gap-2 px-1">
             <Select onValueChange={handleSelectChange}>
-                <SelectTrigger className="w-[240px]">
+                <SelectTrigger className="w-[240px] flex  items-center">
                     <SelectValue placeholder="Wähle das Niveau der Wörter" />
                 </SelectTrigger>
                 <SelectContent>
                     <SelectGroup>
-                        <SelectItem value={EnumWORDLEVELS.A2B2}>A2-B2</SelectItem>
-                        <SelectItem value={EnumWORDLEVELS.C1SICHER}>C1 Sicher!</SelectItem>
-                        <SelectItem value={EnumWORDLEVELS.C1BERUF}>C1 Beruf</SelectItem>
+                        <SelectItem value={EnumWORDLEVELS.A2B2} className="cursor-pointer">
+                            <span className="mr-10">A2-B2</span>
+                            <Badge variant="default" className="bg-primary">
+                                {totalA2B2Cards} Stück
+                            </Badge>
+                        </SelectItem>
+                        <SelectItem value={EnumWORDLEVELS.C1SICHER} className="cursor-pointer">
+                            <span className="mr-10">C1 Sicher!</span>{' '}
+                            <Badge variant="default" className="bg-primary">
+                                {totalC1SicherCards} Stück
+                            </Badge>
+                        </SelectItem>
+                        <SelectItem value={EnumWORDLEVELS.C1BERUF} className="cursor-pointer">
+                            <span className="mr-10">C1 Beruf</span>{' '}
+                            <Badge variant="default" className="bg-primary">
+                                {totalC1BerufCards} Stück
+                            </Badge>
+                        </SelectItem>
                     </SelectGroup>
                 </SelectContent>
             </Select>
