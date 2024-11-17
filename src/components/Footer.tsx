33bd7c624ac721c2c7f081779d7871db96148005
Dmytro Herashchenko <stoczec@gmail.com>
2024-11-17 22:44:40 +0100

    "Added conditional rendering to Footer component based on pathname, displaying copyright info on home route and search bar on other routes."

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,34 +4,46 @@ import React from 'react'
 import { MaxWidthWrapper, SearchBar } from '.'
 import { useCardsStore } from '@/stores'
 import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 
 export const Footer = () => {
     const { searchQuery, filteredCards } = useCardsStore()
+
+    const pathname = usePathname()
+
+    const isHomeRoute = pathname === '/'
     return (
         <footer className="w-full border-t border-gray-200 bg-black/5 py-2">
             <MaxWidthWrapper className="flex justify-center items-center">
-                <div className="w-[320px] flex justify-between items-center">
-                    <SearchBar />
-                    <p
-                        className={cn(
-                            'w-[100px] text-center text-sm text-balance bg-[url("/bg-gray.webp")] bg-cover bg-no-repeat bg-center',
-                            {
-                                'bg-[url("/bg-green.webp")]':
-                                    searchQuery && filteredCards.length > 0,
-                                'bg-[url("/bg-red.webp")]':
-                                    searchQuery && filteredCards.length === 0,
-                            }
-                        )}
-                    >
-                        {searchQuery && filteredCards.length > 1
-                            ? `${filteredCards.length} Wörter gefunden`
-                            : searchQuery && filteredCards.length === 1
-                            ? `${filteredCards.length} Wort gefunden`
-                            : searchQuery && filteredCards.length === 0
-                            ? 'Nichts gefunden!'
-                            : 'Geben Sie ein Wort ein'}{' '}
+                {isHomeRoute ? (
+                    <p className="text-sm text-center text-muted-foreground">
+                        &copy;
+                        {new Date().getFullYear()}
                     </p>
-                </div>
+                ) : (
+                    <div className="w-[320px] flex justify-between items-center">
+                        <SearchBar />
+                        <p
+                            className={cn(
+                                'w-[100px] text-center text-sm text-balance bg-[url("/bg-gray.webp")] bg-cover bg-no-repeat bg-center',
+                                {
+                                    'bg-[url("/bg-green.webp")]':
+                                        searchQuery && filteredCards.length > 0,
+                                    'bg-[url("/bg-red.webp")]':
+                                        searchQuery && filteredCards.length === 0,
+                                }
+                            )}
+                        >
+                            {searchQuery && filteredCards.length > 1
+                                ? `${filteredCards.length} Wörter gefunden`
+                                : searchQuery && filteredCards.length === 1
+                                ? `${filteredCards.length} Wort gefunden`
+                                : searchQuery && filteredCards.length === 0
+                                ? 'Nichts gefunden!'
+                                : 'Geben Sie ein Wort ein'}{' '}
+                        </p>
+                    </div>
+                )}
             </MaxWidthWrapper>
         </footer>
     )
