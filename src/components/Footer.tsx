2dc29c503c78b8973b2c014fafdc1a5248f96c33
Dmytro Herashchenko <stoczec@gmail.com>
2024-11-17 19:01:35 +0100

    "Update Footer component: changed footer height from 14 to 16, added height to button className"

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ import { useCardsStore } from '@/stores'
 export const Footer = () => {
     const { searchQuery, filteredCards } = useCardsStore()
     return (
-        <footer className="w-full h-14 border-t border-gray-200 bg-black/5 flex-grow-0 flex-shrink-0 basis-auto">
+        <footer className="w-full h-16 border-t border-gray-200 bg-black/5 flex-grow-0 flex-shrink-0 basis-auto">
             <MaxWidthWrapper className="flex justify-center items-center">
                 <div className="w-[320px] flex justify-between items-center gap-4">
                     <SearchBar />
@@ -20,7 +20,7 @@ export const Footer = () => {
                                 ? 'outline'
                                 : 'destructive'
                         }
-                        className="text-center"
+                        className="text-center h-9"
                     >
                         {searchQuery && filteredCards.length > 1
                             ? `${filteredCards.length} Wörter gefunden`
