1c8c3e48b70277aa533f23f964972f56944f01a2
Dmytro Herashchenko <stoczec@gmail.com>
2024-11-19 10:56:09 +0100

    "Updated globals.css, page.tsx, Header.tsx, DisclaimerDrawer.tsx, and HintDrawer.tsx with new styles and animations; added keyframes and animation to tailwind.config.ts"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,19 @@ import { Menu } from 'lucide-react'
 export default function Home() {
     return (
         <section className="flex flex-col items-center justify-start gap-10 py-3 flex-grow flex-shrink-0 basis-auto">
-            <p className="text-4xl font-bold mt-5">Willkommen!</p>
+            <p
+                className="text-4xl font-bold mt-5 bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient"
+                style={{ fontFamily: 'MyCustomFont, sans-serif' }}
+            >
+                Willkommen!
+            </p>
             <Image src="/favicon.ico" alt="logo" width={320} height={576} />
             <div className="w-[320px] h-full flex flex-col justify-between items-center gap-4">
                 <Link href="/page/1">
-                    <Button size={'lg'} className="text-lg">
+                    <Button
+                        size={'lg'}
+                        className="text-lg text-white bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-orange-300 dark:focus:ring-orange-800 shadow-lg shadow-orange-500/50 dark:shadow-lg dark:shadow-orange-800/80 font-medium rounded-lg text-center "
+                    >
                         Los geht&lsquo;s!
                     </Button>
                 </Link>
--- src/components/drawers/HintDrawer.tsx
+++ src/components/drawers/HintDrawer.tsx
@@ -19,7 +19,7 @@ export const HintDrawer = () => {
     return (
         <div className="w-full flex justify-center">
             <Drawer>
-                <DrawerTrigger className="w-full font-bold bg-green-500 px-4 py-2 border rounded-lg">
+                <DrawerTrigger className="w-full font-bold text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
                     Tipp
                 </DrawerTrigger>
                 <DrawerContent>
