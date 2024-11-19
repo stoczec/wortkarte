5b78fe99ce7363cc84ae24115dd72ac6a933d7c0
Dmytro Herashchenko <stoczec@gmail.com>
2024-11-19 16:45:53 +0100

    Added font family 'DynaPuffRegular, sans-serif' to About and Contacts pages.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,24 +4,27 @@ import { GrammarTable } from '@/components'
 import { Separator } from '@/components/ui/separator'
 
 export default function About() {
-	return (
-		<section className="flex flex-col items-center justify-between gap-4 py-3 flex-grow flex-shrink-0 basis-auto">
-			<div className="w-[320px] flex flex-col gap-4">
-				<p>
-					<span className="text-3xl font-bold">
-						wort<span className="text-primary">karte </span>-{' '}
-					</span>
-					die den Nutzern dabei hilft, Deutsch zu lernen, indem sie Lernkarten
-					mit assoziativen Bildern und einer farblichen Kategorisierung der
-					Grammatik zur besseren Einprägung verwendet.
-				</p>
-				<Separator className="h-[2px] rounded-xl bg-gray-300" />
-				<p>
-					Auf der Seite werden Wörter je nach Wortklasse farblich hervorgehoben,
-					um ihre Kategorie visuell zu verdeutlichen.
-				</p>
-				<GrammarTable />
-			</div>
-		</section>
-	)
+    return (
+        <section
+            className="flex flex-col items-center justify-between gap-4 py-3 flex-grow flex-shrink-0 basis-auto"
+            style={{ fontFamily: 'DynaPuffRegular, sans-serif' }}
+        >
+            <div className="w-[320px] flex flex-col gap-4">
+                <p>
+                    <span className="text-3xl font-bold">
+                        wort<span className="text-primary">karte </span>-{' '}
+                    </span>
+                    die den Nutzern dabei hilft, Deutsch zu lernen, indem sie Lernkarten mit
+                    assoziativen Bildern und einer farblichen Kategorisierung der Grammatik zur
+                    besseren Einprägung verwendet.
+                </p>
+                <Separator className="h-[2px] rounded-xl bg-gray-300" />
+                <p>
+                    Auf der Seite werden Wörter je nach Wortklasse farblich hervorgehoben, um ihre
+                    Kategorie visuell zu verdeutlichen.
+                </p>
+                <GrammarTable />
+            </div>
+        </section>
+    )
 }
