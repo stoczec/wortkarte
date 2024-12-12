4051034152902ab2a4d00b4128ffa1738c915279
Dmytro Herashchenko <stoczec@gmail.com>
2024-12-12 21:04:34 +0100

    `feat: added ScrollArea and Separator components to display updates`

---
+++ src/data/updates.ts
@@ -0,0 +1,10 @@
+export const updates = [
+    {
+        date: '12.12.24',
+        message: 'Neue Wörter für C1 Beruf hinzugefügt.',
+    },
+    {
+        date: '12.12.24',
+        message: 'Informationen über die letzten Updates wurden auf die Startseite hinzugefügt.',
+    },
+]
