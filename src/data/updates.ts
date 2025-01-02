2d2a14fd6ed312516b42e9d3ac08e876f363842f
Dmytro Herashchenko <stoczec@gmail.com>
2025-01-02 10:03:01 +0100

    `Added 13 new words and 1 update to the C1-Beruf-data.ts and updates.ts files`

--- src/data/updates.ts
+++ src/data/updates.ts
@@ -59,4 +59,8 @@ export const updates = [
         date: '01.01.25',
         message: '7 neue Wörter aus dem Buch C1 Aspekte Beruf wurden hinzugefügt.',
     },
+    {
+        date: '02.01.25',
+        message: '13 neue Wörter aus dem Buch C1 Aspekte Beruf wurden hinzugefügt.',
+    },
 ]
